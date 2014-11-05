750d7b1986f76cf09a7f3dbfa117567351046e6d
cruzalfredo <alf.bibiano@gmail.com>
2014-11-05 14:25:46 -0600

    cambio de accesos a la base de datos

--- examen/lib/db.php
+++ examen/lib/db.php
@@ -2,7 +2,7 @@
 $db_name="examen";
 $db_sever="localhost";
 $db_user="root";
-$db_pass="qwerty";
+$db_pass="";
 $db_connection=mysql_connect($db_sever,$db_user,$db_pass) or die ("Error en la conección a la Base de datos");
 mysql_select_db($db_name,$db_connection) or die ("Error al selecionar la BD");
 mysql_query("SET NAMES utf8");
